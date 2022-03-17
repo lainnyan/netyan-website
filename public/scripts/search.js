8285c6ec7b2eab45f3dae8509b9750093acf2e92
Atakashi <petrencko0607@gmail.com>
2022-03-17 22:58:50 +0500

    changed size of builds

--- public/scripts/search.js
+++ public/scripts/search.js
@@ -1,16 +1,27 @@
 const searchbar = document.getElementById("build-search");
 const buildContainers = document.getElementsByClassName("img-container");
 
-searchbar.addEventListener("input", function () {
+/*searchbar.addEventListener("input", function () {
     for (let i = 0; i < buildContainers.length; i++) {
         const buildContainer = buildContainers[i];
         buildContainer.style.display = "block";
-    }
-    for (let i = 0; i < buildContainers.length; i++) {
-        const buildContainer = buildContainers[i];
         var charID = buildContainer.id.substring(buildContainer.id.indexOf("-") + 1);
-        if (!charID.includes(searchbar.value)) {
-            buildContainer.style.display = "none";
+        for (let j = 0; j < tagsArray.length; j++) {
+            const tagsArrayElement = tagsArray[j];
+            if (tagsArrayElement.name == charID) {
+                for (let k = 0; k < tagsArrayElement.tags.length; k++) {
+                    const tags = tagsArrayElement.tags[k];
+                }
+            }
+                if (!tagsArrayElement.tags.includes(searchbar.value))
+                    buildContainer.style.display = "none";
         }
     }
-});
+});*/
+
+tagsArray = [
+    {
+        name: "keqing",
+        tags: ["Кэ Цин", "кецин", "кеца", "кека", "Keqing", "Ke Qing"],
+    },
+];
